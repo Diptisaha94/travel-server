365de795f55d478bd4a8362360fba92b0e5cbe80
Dipti saha <sahadipti322@gmail.com>
2022-11-08 07:39:20 -0500

    service specific data loaded

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
   })
   
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://travel:AdminTul@cluster0.yyxrk8i.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
@@ -18,12 +18,24 @@ async function run() {
       const database = client.db("tour");
       const serviceCollection = database.collection("services");
       // Query for a movie that has the title 'The Room'
+      app.get('/service',async(req,res)=>{
+        const query = {};
+      const cursor = serviceCollection.find(query);
+    const services = await cursor.limit(3).toArray();
+    res.send(services);
+      });
       app.get('/services',async(req,res)=>{
         const query = {};
       const cursor = serviceCollection.find(query);
     const services = await cursor.toArray();
     res.send(services);
-      })
+      });
+      app.get('/services/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const service = await serviceCollection.findOne(query);
+        res.send(service);
+    });
     } finally {
     
     }
