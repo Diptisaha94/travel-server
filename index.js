4e5cc128800085b9559b047bede3976252e36d69
Dipti saha <sahadipti322@gmail.com>
2022-11-10 09:46:20 -0500

    issue solve

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('server is running!')
   })
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
